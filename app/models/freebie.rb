1b89ed7c91a7f630aebb7cb6902c6e82361e5b3c
Ahnaf <ahrmada95@gmail.com>
2022-08-17 21:50:42 -0400

    code challenge completeo

--- app/models/freebie.rb
+++ app/models/freebie.rb
@@ -1,3 +1,8 @@
 class Freebie < ActiveRecord::Base
-
+    belongs_to :company #note singular since it can only belong to one company 
+    belongs_to :dev #note singular since it can only belong to one dev
+  
+    def print_details 
+        return "#{dev.name} owns a #{self.item_name} from #{company.name}"
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,20 @@
 puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
-Company.create(name: "Facebook", founding_year: 2004)
-Company.create(name: "Dunder Mifflin", founding_year: 2002)
-Company.create(name: "Enron", founding_year: 1995)
+google = Company.create(name: "Google", founding_year: 1998)
+facebook = Company.create(name: "Facebook", founding_year: 2004)
+dunder = Company.create(name: "Dunder Mifflin", founding_year: 2002)
+enron = Company.create(name: "Enron", founding_year: 1995)
 
 puts "Creating devs..."
-Dev.create(name: "Rick")
-Dev.create(name: "Morty")
-Dev.create(name: "Mr. Meseeks")
-Dev.create(name: "Gazorpazop")
+rick = Dev.create(name: "Rick")
+morty = Dev.create(name: "Morty")
+meseeks = Dev.create(name: "Mr. Meseeks")
+gazor = Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
 
-# ***************************************************************
-# * TODO: create freebies! Remember, a freebie belongs to a dev *
-# * and a freebie belongs to a company.                         *
-# ***************************************************************
-# Create freebies Here
+Freebie.create(item_name: "SalesForce Fidget Cube", value: 8, dev_id: gazor.id, company_id: enron.id)
+Freebie.create(item_name: "Google Mug", value: 15, dev_id: meseeks.id, company_id: dunder.id)
+Freebie.create(item_name: "Citi Bank Shirt", value: 5, dev_id: morty.id, company_id: enron.id)
+Freebie.create(item_name: "Apple Apple Sticker", value: 20, dev_id: rick.id, company_id: google.id)
 
 puts "Seeding done!"
